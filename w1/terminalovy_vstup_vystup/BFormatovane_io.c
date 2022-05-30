146975415cad887e4f6c75f27220e39c2a1fe3ac
stemberk <stemberk@spsmb.cz>
2022-05-30 12:15:23 +0200

    ukol +

--- w1/terminalovy_vstup_vystup/BFormatovane_io.c
+++ w1/terminalovy_vstup_vystup/BFormatovane_io.c
@@ -5,6 +5,19 @@
 // scanf("%d", &i)  - přečte y klávesnice celé číslo a uloží ho do proměnné i
 // výstup:  printf() Např.
 // printf("%d", i) - vytiskne i
+// řídící řetězec formátu:
+// c    - znak
+// d    - signed int
+// ld   - signed long
+// u    - unsigned int
+// lu   - unsigned long
+// f    - float (pro printf také double)
+// Lf   - long double
+// lf   - double
+// x    - hexadecimální číslo s malými písmeny
+// X    - hexadecimální číslo s velkými písmeny
+// o    - osmičkové číslo
+// s    - řetězec
 
 // Napište program, který přečte z klávesnice 2 čísla, vytiskne je v obráceném
 // pořadí a vytiskne jejich součet
---
+++ w1/terminalovy_vstup_vystup/CUkol.c
@@ -0,0 +1,9 @@
+//
+// Created by stemberk on 30.05.2022.
+//
+// 1. Napište program, který vypíše přesně následující text:
+// James Bond \ "Agent 007" \ # 150% zaruka # /
+// 2. Napište program, který přečte znak a vypíše znak s ASCII hodnotou o jednu
+// vyšší. Např.: vstup: A, výstup: B (ASCII 66)
+// 3. Napište program, který přečte celé dekadické číslo (0-255) a vypíše jeho
+// hexadecimální hodnotu dvouznakově.
