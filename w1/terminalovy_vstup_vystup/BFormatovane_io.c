e13f2a361fe31abedc13e3f6a908a77920bdf4d9
stemberk <stemberk@spsmb.cz>
2022-05-30 11:59:08 +0200

    ukol +

---
+++ w1/terminalovy_vstup_vystup/BFormatovane_io.c
@@ -0,0 +1,11 @@
+//
+// Created by stemberk on 30.05.2022.
+//
+// vstup:   scanf(). Např.
+// scanf("%d", &i)  - přečte y klávesnice celé číslo a uloží ho do proměnné i
+// výstup:  printf() Např.
+// printf("%d", i) - vytiskne i
+
+// Napište program, který přečte z klávesnice 2 čísla, vytiskne je v obráceném
+// pořadí a vytiskne jejich součet
+
