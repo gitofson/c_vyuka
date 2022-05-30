c5291c389300763cddda2270c094ae29e5d72eed
stemberk <stemberk@spsmb.cz>
2022-05-30 11:11:51 +0200

    Uvod

---
+++ w1/datove_typy/AUvod.c
@@ -0,0 +1,24 @@
+//
+// Created by stemberk on 30.05.2022.
+//
+// Jazyk C disponuje následujícími typy:
+// celočíselné:
+// int               - velikost záleží na architektuře),
+// long int (long)   - 64 bit
+// short int (short) - 16 bit
+// char              - 8 bit
+//
+// pomocí slova signed (default) a unsigned rozlišujeme, zda rozsah čísla bude
+// od -2^(n-1) do +2^(n-1)-1 pro signed a od 0 do +2^n-1 pro unsigned
+
+// plovoucí řídová čárka:
+// float             - 64bit
+// double            - 128bit
+
+// C neposkytuje datový typ boolean. Namísto toho používáme int, který znamená
+// 0                     - false
+// cokoliv jiného, než 0 - true;
+# include <stdio.h>
+int main(int argc, char ** argv) {
+    printf("hello\n");
+}
