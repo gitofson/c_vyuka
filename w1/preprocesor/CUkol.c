50554534e0c9d95bb61392407d55d41c27cf345a
stemberk <stemberk@spsmb.cz>
2022-06-02 12:07:21 +0200

    preprocessor

---
+++ w1/preprocesor/CUkol.c
@@ -0,0 +1,18 @@
+//
+// Created by stemberk on 02.06.2022.
+//
+// 1, Napište program, který sečte N prvních přirozených čísel a vypíše
+// např.: Soucet prvnich 5 čísel je 15. N definujte jako symbolickou
+// konstantu
+
+// 2, čtěte znaky z klávesnice až do EOLN, které si definujete. Po skončení
+// čtení vypište počet zadaných číslic. Dále opište jen zadaná písmena,
+// která převeďte na velká. Využijte maker isdigit(), isalpha() A toupper()
+// ze souboru ctype.h.
+
+// 3, napište makro na_treti(x), které bude počítat třetí mocninu. Vyzkoušejte
+// ho na výrazech :
+// na_treti(3)
+// na_treti(i)
+// na_treti(2 + 3)
+// na_treti(i * j + 1)
