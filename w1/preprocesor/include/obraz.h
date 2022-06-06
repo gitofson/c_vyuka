df8ae8e06136321545588ed9d44e98878e0942b5
stemberk <stemberk@spsmb.cz>
2022-06-06 11:05:45 +0200

    ctype.h

---
+++ w1/preprocesor/include/obraz.h
@@ -0,0 +1,11 @@
+//
+// Created by stemberk on 06.06.2022.
+//
+
+#ifndef C_VYUKA_OBRAZ_H
+#define C_VYUKA_OBRAZ_H
+
+#define RADKY_OBRAZOVKY   25
+#define SLOUPCE_OBRAZOVKY 80
+
+#endif //C_VYUKA_OBRAZ_H
