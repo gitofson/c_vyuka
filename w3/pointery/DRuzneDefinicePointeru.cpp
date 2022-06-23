20345788cbabe124d8215400b0cfbd086a0ecbaf
stemberk <stemberk@spsmb.cz>
2022-06-23 11:03:52 +0200

    pointery

---
+++ w3/pointery/DRuzneDefinicePointeru.cpp
@@ -0,0 +1,11 @@
+//
+// Created by stemberk on 23.06.2022.
+//
+// Příklady:
+// int x;           x je typu int
+// float *y;        y je pointer na typ float
+// double *z();     z je funkce vracející pointer na double
+// int *(*v)();     v je pointer na funkci vracející pointer na int
+
+// double (* f[])();f je pole pointerů na funkce vracející typ double
+// double (* f())[];f je funkce vracející  pointer na pole prvků typu double
